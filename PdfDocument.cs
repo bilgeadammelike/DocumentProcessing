a2a9b19ccc7631c6e0d3578dae3c0759220eeeb1
kadir <kadir@kadir.com>
2024-03-20 06:08:56 +0300

    Add project files.

---
+++ PdfDocument.cs
@@ -0,0 +1,72 @@
+﻿using Syncfusion.Pdf;
+using Syncfusion.Pdf.Parsing;
+using System.Threading.Channels;
+
+namespace DocumentProcessing
+{
+    public class PdfDocument : IProcessDocument
+    {
+        public ChannelReader<string> ReaderChannel { get; }
+
+        public PdfDocument(ChannelReader<string> readerChannel)
+        {
+            ReaderChannel = readerChannel;
+        }       
+
+        public void ExtractText()
+        {
+
+            while (true)
+            {
+                try
+                {
+                    string? filePath = string.Empty;
+
+                    if (ReaderChannel.Completion.IsCompleted)
+                    {
+                        break;
+                    }
+
+                    bool success = ReaderChannel.TryRead(out filePath);
+                    if(false == success || string.IsNullOrEmpty(filePath))
+                    {
+                        Thread.Sleep(TimeSpan.FromSeconds(1));
+                        continue;
+                    }
+                    
+                    //Get stream from an existing PDF document. 
+                    FileStream docStream = new FileStream(Path.GetFullPath(filePath), FileMode.Open, FileAccess.Read);
+
+                    //Load the PDF document.
+                    PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
+
+                    //Loading page collections.
+                    PdfLoadedPageCollection loadedPages = loadedDocument.Pages;
+
+                    string extractedText = string.Empty;
+
+                    //Extract text from existing PDF document pages.
+                    foreach (PdfLoadedPage loadedPage in loadedPages)
+                    {
+                        extractedText += loadedPage.ExtractText();
+                    }
+
+                    //Close the document.
+                    loadedDocument.Close(true);
+                }
+                catch (ChannelClosedException)
+                {
+                    break;
+                }
+                catch(Syncfusion.Pdf.PdfInvalidPasswordException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿
+using System.Threading.Channels;
+
+namespace DocumentProcessing
+{
+    internal class Program
+    {
+        public static int  TaskCount = 16;        
+
+        static void Main(string[] args)
+        {
+            UnboundedChannelOptions options = new UnboundedChannelOptions
+            {
+                AllowSynchronousContinuations = true,
+                SingleReader = false,
+                SingleWriter = true
+            };
+            var channel = Channel.CreateUnbounded<string>(options);
+            var taskList = new List<Task>();
+
+            Console.WriteLine("Veri İşleme Başlıyor!");
+
+            var taskReader = Task.Factory.StartNew(() => FileReader(channel.Writer) , 
+                CancellationToken.None, 
+                TaskCreationOptions.DenyChildAttach, 
+                TaskScheduler.Default);
+
+            taskList.Add(taskReader);
+
+            for (int i = 0; i < TaskCount; ++i)
+            {
+                var taskConsumer = Task.Factory.StartNew(() =>
+                {
+                    IProcessDocument document = new PdfDocument(channel.Reader);
+                    document.ExtractText();
+                },
+                    CancellationToken.None,
+                    TaskCreationOptions.DenyChildAttach,
+                    TaskScheduler.Default);
+
+                taskList.Add(taskConsumer);
+            }
+
+            Task.WaitAll(taskList.ToArray());
+
+            Console.WriteLine("Veri İşleme Bitti...");
+        }
+
+        private static void FileReader(ChannelWriter<string> writingChannel)
+        {
+            var files = Directory.GetFiles($@"..\..\..\Assets\PdfFiles\", "*.pdf", SearchOption.TopDirectoryOnly);
+            foreach (var filePath in files)
+            {
+                var success = writingChannel.TryWrite(filePath);
+                if (false == success)
+                {
+                    Console.WriteLine(@$"Channel Writing Failed...{filePath}");
+                }
+            }
+
+            Thread.Sleep(TimeSpan.FromSeconds(10));       
+
+            writingChannel.Complete();
+        }
+    }
+}
